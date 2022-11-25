79be1d8cdfc2804d6fd392fbe74bf93f2cb1b671
rafasanzvi <rafasv41@gmail.com>
2022-11-25 15:35:40 +0100

    Code refactoring in different functions

--- src/Principal/GreaterNumber.java
+++ src/Principal/GreaterNumber.java
@@ -8,22 +8,27 @@ public class GreaterNumber {
 
     public static void main(String[] args) throws IOException{
 
+        System.out.println("Write the first number: ");
+        Integer number1 = chargeNumber();
+
+        System.out.println("Write the second number: ");
+        Integer number2 = chargeNumber();
+
+        System.out.println("Write the third number: ");
+        Integer number3 = chargeNumber();
+
+        calculateGreaterNumber(number1, number2, number3);
+        calculateLowerNumber(number1, number2, number3);
+    }
+
+    private static Integer chargeNumber() throws IOException {
         InputStreamReader getInput = new InputStreamReader(System.in);
         BufferedReader readerTheInputs = new BufferedReader(getInput);
 
-        System.out.println("Write the first number: ");
         String strNumber1 = readerTheInputs.readLine();
-        Integer number1 = Integer.parseInt(strNumber1);
+        Integer number = Integer.parseInt(strNumber1);
 
-        System.out.println("Write the second number: ");
-        String strNumber2 = readerTheInputs.readLine();
-        Integer number2 = Integer.parseInt(strNumber2);
-
-        System.out.println("Write the third number: ");
-        String strNumber3 = readerTheInputs.readLine();
-        Integer number3 = Integer.parseInt(strNumber3);
-
-        calculateGreaterNumber(number1, number2, number3);
+        return number;
     }
 
     private static void calculateGreaterNumber(Integer number1, Integer number2, Integer number3) {
@@ -37,6 +42,10 @@ public class GreaterNumber {
         }
 
         System.out.println("The greater number is: " + greaterNumber);
+
+    }
+
+    private static void calculateLowerNumber(Integer number1, Integer number2, Integer number3) {
 
         Integer lowerNumber = number1;
 
